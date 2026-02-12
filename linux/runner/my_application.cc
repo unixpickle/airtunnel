756bc9cf79e6502573e56601d3dcdbeee3deea80
Alex Nichol <unixpickle@gmail.com>
2026-02-12 08:57:51 -0500

    Rename app display name to AirTunnel

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "airtunnel_scaffold");
+    gtk_header_bar_set_title(header_bar, "AirTunnel");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "airtunnel_scaffold");
+    gtk_window_set_title(window, "AirTunnel");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
